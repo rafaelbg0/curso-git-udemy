bc09dcffff5e5a4e9b05df3c3865babf0278dfc6
Rafael <rafael.bg0@gmail.com>
2019-11-05 22:04:04 -0300

    Primeiro commit teste

---
+++ projeto-teste-git/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projeto_teste_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alô mundo!!");
+
+            Console.WriteLine();
+        }
+    }
+}
